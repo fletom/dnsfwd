690bcfcf6011b624c8482bb08cdc32acce67892e
Fletcher Tomalty <fletcher@tomalty.com>
2014-03-14 02:14:46 -0400

    Add caching (expires with TTL) using a local memcached server.

--- domfwd.py
+++ domfwd.py
@@ -1,12 +1,27 @@
+import socket
 import dns.exception
 import dns.resolver
+import pymemcache.client
 from werkzeug.wrappers import Request
 
 resolver = dns.resolver.Resolver()
 resolver.lifetime = 0.500
 
+# Try a local memcached, but do without if we can't connect
+cache = pymemcache.client.Client(('localhost', 11211))
+try:
+	cache.get('test')
+except socket.error:
+	cache = None
+
+
 def lookup_fwd(domain, rdepth = 1):
 	"""Look up the forwarding address for a domain."""
+	
+	if cache:
+		cached = cache.get(domain)
+		if cached is not None:
+			return cached
 	
 	try:
 		answer = resolver.query(domain, 'CNAME', tcp = True)
@@ -18,13 +33,12 @@ def lookup_fwd(domain, rdepth = 1):
 	):
 		return None
 	
-	ttl = answer.rrset.ttl
 	cname = answer[0].to_text().rstrip('.')
 	
 	ending = '.domfwd.com'
 	
 	if not cname.endswith(ending):
-		# It could be a CNAME that points to our CNAME, which should work but not get stuck in a loop
+		# It could be an intermediary CNAME that points to our CNAME, which should work but not get stuck in a loop
 		
 		rdepth += 1
 		if rdepth > 3:
@@ -36,6 +50,9 @@ def lookup_fwd(domain, rdepth = 1):
 	
 	if fwd_to == 'unwww' and domain.startswith('www.'):
 		fwd_to = domain[4:]
+	
+	if cache:
+		cache.set(domain, fwd_to, answer.rrset.ttl)
 	
 	return fwd_to
 
